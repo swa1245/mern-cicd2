ac7a3c002b88ae5d9c8944cd079c70d58702e56b
Swaraj Patil <161611230+swa1245@users.noreply.github.com>
2025-07-22 22:31:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000/api/tasks";
+const API = "https://35.154.133.207/api/tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
